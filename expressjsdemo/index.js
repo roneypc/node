3bafa2a9a37453dc247e5cd6342e991f9cd55cc9
Jorge Roberto Neyra <roneypc@gmail.com>
2016-11-17 19:31:21 +0100

    more samples

--- expressjsdemo/index.js
+++ expressjsdemo/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 var things = require('./things.js'); 
 
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer(); 
-
 // se indica el motor que se utiliza )ejs)
 app.set('view engine', 'ejs');
 // carpeta por defecto donde se ponen las vistas (./views)
@@ -15,15 +11,6 @@ app.set('views', './views');
 app.use('/', things);
 app.use(express.static('public'));
 app.use(express.static('images'));
-
-// for parsing application/json
-app.use(bodyParser.json());
-
-// for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// for parsing multipart/form-data
-app.use(upload.array());
 
 //app.use(express.static('public'));
 
